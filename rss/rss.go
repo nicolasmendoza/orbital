cc6f4402659d492103cb38b84f36c2cd38d2291b
Nicolás Mendoza <niccolasmendoza@gmail.com>
2019-03-17 06:12:18 -0300

    More testssss :o))))  :rocket:

--- rss/rss.go
+++ rss/rss.go
@@ -1,10 +1,88 @@
 package rss
 
 import (
+	"encoding/xml"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 )
+
+// http://www.w3schools.com/rss/rss_syntax.asp
+// http://www.w3schools.com/rss/rss_channel.asp
+// RSS Version 2.
+type RSS struct {
+	XMLName xml.Name `xml:"rss"`
+	Version string   `xml:"version,attr"`
+	// Required
+	Title       string `xml:"channel>title"`
+	Link        string `xml:"channel>link"`
+	Description string `xml:"channel>description"`
+	// Optional
+	PubDate  string    `xml:"channel>pubDate"`
+	ItemList []XMLItem `xml:"channel>item"`
+}
+
+// http://www.w3schools.com/rss/rss_item.asp
+// http://stackoverflow.com/questions/7220670/difference-between-description-and-contentencoded-tags-in-rss2
+// https://groups.google.com/d/topic/golang-nuts/uBMo1BpaQCM
+type XMLItem struct {
+	// Required
+	Title       string        `xml:"title"`
+	Link        string        `xml:"link"`
+	Description template.HTML `xml:"description"`
+	// Optional
+	Content  template.HTML `xml:"encoded"`
+	PubDate  string        `xml:"pubDate"`
+	Comments string        `xml:"comments"`
+}
+
+type Document struct {
+	Title       string
+	Description template.HTML
+	Link        string
+	//Author      string
+	//Category    string
+	PubDate string
+	Done    bool
+}
+
+func getDocument(url string, checkCache bool) error {
+	body, err := readBody(url, checkCache)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+
+	}
+
+	docXML := parserXML(body)
+	if docXML != nil {
+		//Iterating items in document.
+		for _, value := range docXML.ItemList {
+
+			document := new(Document)
+			document.Title = value.Link
+			document.Link = value.Link
+			document.PubDate = value.PubDate
+			document.Done = false
+
+		}
+	}
+
+	println(body)
+	return nil
+}
+
+func parserXML(doc []byte) *RSS {
+	//Processing XML
+	var docXML RSS
+	if err := xml.Unmarshal(doc, &docXML); err != nil { // TODO FIXME. STORE THE XML FILE when error ocurred.
+		log.Fatal("Cannot marshall XML (RSS) File...")
+		return nil
+	}
+
+	return &docXML
+}
 
 func readBody(url string, checkCache bool) ([]byte, error) {
 	// HTTP GET to the resource, with conditional value.
@@ -22,15 +100,4 @@ func readBody(url string, checkCache bool) ([]byte, error) {
 	}
 	return body, nil
 
-}
-
-func getDocument(url string, checkCache bool) error {
-	body, err := readBody(url, checkCache)
-	if err != nil {
-		log.Println(err.Error())
-		return err
-
-	}
-	fmt.Println("Vamooo que vamoooo", string(body))
-	return nil
 }
--- rss/rss_test.go
+++ rss/rss_test.go
@@ -1,10 +1,14 @@
 package rss
 
-import "testing"
+import (
+	"io/ioutil"
+	"testing"
+)
 
 const (
 	url = "https://www.contratos.gov.co/Archivos/RSSFolder/RSSFiles/rssFeed-10000000.xml"
 )
+
 func TestReadBody(t *testing.T) {
 	body, err := readBody(url, false)
 	if err != nil {
@@ -16,9 +20,25 @@ func TestReadBody(t *testing.T) {
 	}
 }
 
-func TestGetDocument(t *testing.T){
+func TestGetDocument(t *testing.T) {
 	err := getDocument(url, false)
-	if err !=nil{
+	if err != nil {
 		t.Errorf("Error getting XML Document: %v", err.Error())
+	}
+}
+
+func TestParserXMLtoDocument(t *testing.T) {
+	// Openning Example XML...
+	xmlFile, err := ioutil.ReadFile("./rss_test.xml")
+	if err != nil {
+		t.Fatalf("Cannot open test file...%v. %v", xmlFile, err.Error())
+	}
+
+	rss := parserXML(xmlFile)
+	if rss == nil{
+		t.Error("Error parsing HTML")
+	}
+	if rss.Version != "2.0"{
+		t.Error("Invalid XML version. Error parsing file...")
 	}
 }
